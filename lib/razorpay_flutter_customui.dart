cb64236b4bb1d993bcd76926273d899f06ad97f7
raviworkrush <110544877+raviroyalmart@users.noreply.github.com>
2022-11-16 13:25:40 +0530

    added dart comments

--- lib/razorpay_flutter_customui.dart
+++ lib/razorpay_flutter_customui.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:eventify/eventify.dart';
 import 'package:flutter/services.dart';
 
+/// [Razorpay] is the main class of the plugin. It provides methods to open the
+/// Razorpay checkout form and listen to events.
 class Razorpay {
   // Response codes from platform
   static const _CODE_PAYMENT_SUCCESS = 0;
@@ -29,70 +31,82 @@ class Razorpay {
     _eventEmitter = new EventEmitter();
   }
 
+  /// [getPaymentMethods] is used to get the list of payment methods available.
   Future<Map<dynamic, dynamic>> getPaymentMethods() async {
     final Map<dynamic, dynamic> paymentMethodsObj =
         await _channel.invokeMethod('getPaymentMethods');
     return paymentMethodsObj;
   }
 
+  /// [getAppsWhichSupportUpi] is used to get the list of apps which support [UPI](https://www.npci.org.in/what-we-do/upi/product-overview).
   Future<dynamic> getAppsWhichSupportUpi() async {
     final paymentMethodsObj =
         await _channel.invokeMethod('getAppsWhichSupportUpi');
     return paymentMethodsObj;
   }
 
+  /// [getCardsNetwork] is used to get the list of cards network available.
   Future<String> getCardsNetwork(String cardNumber) async {
     final String cardNetwork =
         await _channel.invokeMethod('getCardNetwork', cardNumber);
     return cardNetwork;
   }
 
+  /// [isCredAppAvailable] is used to check if the Cred app is installed on the device.
   Future<bool> isCredAppAvailable() async {
     final bool isCredAppPresent =
         await _channel.invokeMethod('isCredAppAvailable');
     return isCredAppPresent;
   }
 
+  /// [getWalletLogoUrl] is used to get the wallet logo url.
   Future<String> getWalletLogoUrl(String walletName) async {
     final walletLogoUrl =
         await _channel.invokeMethod('getWalletLogoUrl', walletName);
     return walletLogoUrl;
   }
 
+  /// [getBankLogoUrl] is used to get the bank logo url.
   Future<String> getBankLogoUrl(String bankName) async {
-    final bankLogoUrl =
-        await _channel.invokeMethod('getBankLogoUrl', bankName);
+    final bankLogoUrl = await _channel.invokeMethod('getBankLogoUrl', bankName);
     return bankLogoUrl;
   }
 
+  /// [getSubscriptionAmount] is used to get the subscription amount.
   Future<dynamic> getSubscriptionAmount(String subscriptionId) async {
     final dynamic subscriptionAmount =
         await _channel.invokeMethod('getSubscriptionAmount', subscriptionId);
     return subscriptionAmount;
   }
 
+  /// [getCardNetworkLength] is used to get the card network length.
   Future<dynamic> getCardNetworkLength(String network) async {
     final dynamic cardNetworkLength =
         await _channel.invokeMethod('getCardNetworkLength', network);
     return cardNetworkLength;
   }
 
+  /// [isValidCardNumber] is used to check if the card number is valid.
+  /// [network] is the card network.
   Future<bool> isValidCardNumber(String network) async {
     final dynamic isValidCard =
         await _channel.invokeMethod('isValidCardNumber', network);
     return isValidCard;
   }
 
+  /// [isValidVpa] is used to check if the vpa is valid.
   Future<Map<dynamic, dynamic>> isValidVpa(String vpa) async {
     final dynamic isValidVpa = await _channel.invokeMethod('isValidVpa', vpa);
     return isValidVpa;
   }
 
-
+  /// [initilizeSDK] is used to initialize the SDK.
+  /// [key] is the API key.
   initilizeSDK(String key) {
     _channel.invokeMethod('initilizeSDK', key);
   }
 
+  /// [submit] is used to submit the payment.
   submit(Map<String, dynamic> options) async {
     Map<String, dynamic> validationResult = _validateOptions(options);
 
@@ -111,6 +125,7 @@ class Razorpay {
     _handleResult(response);
   }
 
+  /// [payWithCred] is used to pay with CRED
   payWithCred(Map<String, dynamic> options) async {
     Map<String, dynamic> validationResult = _validateOptions(options);
 
@@ -163,6 +178,7 @@ class Razorpay {
     }
   }
 
+  /// [clear] is used to clear the event listeners.
   void clear() {
     _eventEmitter.clear();
   }
@@ -180,6 +196,10 @@ class Razorpay {
   }
 }
 
+/// [PaymentSuccessResponse] is the response object for the payment success event.
+/// [paymentId] is the payment id.
+/// [orderId] is the order id.
+/// [signature] is the signature.
 class PaymentSuccessResponse {
   String paymentId;
   String orderId;
@@ -196,6 +216,9 @@ class PaymentSuccessResponse {
   }
 }
 
+/// [PaymentFailureResponse] is the response object for the payment failure event.
+/// [code] is the error code.
+/// [message] is the error message.
 class PaymentFailureResponse {
   int code;
   String message;
@@ -205,6 +228,6 @@ class PaymentFailureResponse {
   static PaymentFailureResponse fromMap(Map<dynamic, dynamic> map) {
     var code = map["http_status_code"] as int;
     var message = map["metadata.reason"] as String;
-    return new PaymentFailureResponse(code, message);
+    return PaymentFailureResponse(code, message);
   }
 }
